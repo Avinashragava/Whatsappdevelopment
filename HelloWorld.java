a1e190379d11845ba3d75394773d7c9b087340c8
Avinashragava <89409184+Avinashragava@users.noreply.github.com>
2025-01-22 21:49:04 +0530

    Update HelloWorld.java
    Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
 public static void main(string[] args){
 System.out.println("Hello World");
+System.out.println("Pushed Hello World program for jenkins verification");
 }
 }
